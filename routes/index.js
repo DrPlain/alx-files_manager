e65b033880cea07894da5e17cc435d24eeae29e0
Gideon Obiasor <gideonoiasor@gmail.com>
2023-06-24 16:58:52 +0100

    Tasks 5

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import { getStatus, getStats } from '../controllers/AppController';
 import postNew from '../controllers/UsersController';
 import { getConnect, getDisconect, getMe } from '../controllers/AuthController';
+import postUpload from '../controllers/FilesController';
 
 const router = express.Router();
 
@@ -11,5 +12,6 @@ router.post('/users', postNew);
 router.get('/connect', getConnect);
 router.get('/disconnect', getDisconect);
 router.get('/users/me', getMe);
+router.post('/files', postUpload);
 
 export default router;
